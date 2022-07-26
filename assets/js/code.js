d88a70a32e7994f008b957b1d77a706812b2c212
JuanLeon123 <juandiego.ledo@gmail.com>
2022-07-25 20:38:20 -0500

    juan

---
+++ assets/js/code.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('#menu-btn');
+const menu = document.querySelector('#sidemenu');
+btn.addEventListener('click', e => {
+    menu.classList.toggle("menu-expanded");
+    menu.classList.toggle("menu-collapsed");
+    document.querySelector('body').classList.toggle('body-expanded');
+});
+
+
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="mascota">WELCOME TO MIAMI BIT HOTEL</i>',
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
